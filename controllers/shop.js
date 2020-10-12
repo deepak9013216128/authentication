fb34e398bb3e08b8d782241c6e0c32435b2e2e66
deepak9013216128 <deepak9013216128@gmail.com>
2020-10-12 09:51:21 +0530

    protect route from csrf attack using csrf token

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,7 +9,6 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: 'All Products',
         path: '/products',
-        isAuthenticated: req.session.isLoggedIn
       })
     })
     .catch(err => console.log(err))
@@ -24,7 +23,6 @@ exports.getProduct = (req, res, next) => {
           product,
           pageTitle: product.title,
           path: '/products',
-          isAuthenticated: req.session.isLoggedIn
         })
       }
       return res.redirect('/products')
@@ -39,7 +37,6 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        isAuthenticated: req.session.isLoggedIn
       })
     })
     .catch(err => console.log(err))
@@ -55,7 +52,6 @@ exports.getCart = (req, res, next) => {
         pageTitle: 'Your Cart',
         path: '/cart',
         products,
-        isAuthenticated: req.session.isLoggedIn
       })
     })
     .catch(err => console.log(err))
@@ -96,7 +92,7 @@ exports.postOrders = (req, res, next) => {
       }))
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user
         },
         products: products
@@ -118,7 +114,6 @@ exports.getOrders = (req, res, next) => {
         pageTitle: 'Your Orders',
         path: '/orders',
         orders: orders,
-        isAuthenticated: req.session.isLoggedIn
       })
     })
     .catch(err => console.log(err))
