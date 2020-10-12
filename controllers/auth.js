6fead604436e8eec46063f09bf5b8db985bc2e10
deepak9013216128 <deepak9013216128@gmail.com>
2020-10-12 15:45:57 +0530

    send email on signup using sendgrid

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer')
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: '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',
+  }
+}))
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -82,7 +90,14 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login')
+          return transporter.sendMail({
+            to: email,
+            from: 'deepak9692031@gmail.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          })
         })
+        .catch(err => console.log(err))
     })
     .catch(err => console.log(err))
 }
